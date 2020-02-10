cb4421dd64c8e842ef17cafe7d0cbfa9875d371b
CarlaOnate <carlaonategardella@gmail.com>
2020-02-10 11:41:30 -0600

    Prueba configuracion

--- starter-code/app.js
+++ starter-code/app.js
@@ -63,6 +63,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 const index = require('./routes/index')
 app.use('/', index)
+// app.use('/', require('./routes/suscribe.routes'))
 app.use('/auth', require('./routes/auth'))
 
 
---
+++ starter-code/controllers/suscribe.controller.js
@@ -0,0 +1,4 @@
+
+// exports.suscribeView = () => {
+//   res.render('index', {msg: 'El correo fue registrado exitosamente'})
+// }
