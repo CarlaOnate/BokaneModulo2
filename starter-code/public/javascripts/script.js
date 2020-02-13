1d16b5d7bb636b11a9528c88f472768e8dea15b0
Jorge Aguilar <jaguilar.ice@gmail.com>
2020-02-12 21:38:55 -0600

    adding views

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -5,52 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
   style: 'mapbox://styles/mapbox/satellite-streets-v9',
   center: [-93.664719, 15.844154],
   zoom: 15
+}
+)
 })
-
-var marker = new mapboxgl.Marker()
-  .setLngLat([-93.664719, 15.844154])
-  .addTo(map);
-
-}, false);
-
-
-
-/*
-
-document.getElementById('check-room1').addEventListener('click',()=>{
-  if(document.getElementById('check-room1').checked){
-    console.log('checado')
-  document.getElementById('check-room2').checked=false;
-  document.getElementById('check-room3').checked=false;
-  }else{
-  document.getElementById('check-room2').checked=true;
-  document.getElementById('check-room3').checked=true;
-  }
-
-});
-
-
-document.getElementById('check-room2').addEventListener('click',()=>{
-  if(document.getElementById('check-room2').checked){
-  document.getElementById('check-room1').checked=false;
-  document.getElementById('check-room3').checked=false;
-  }else{
-  document.getElementsById('check-room1').checked=true;
-  document.getElementsById('check-room3').checked=true;
-  }
-
-});
-
-
-document.getElementsById('check-room3').addEventListener('click',()=>{
-  if(document.getElementById('check-room3').checked){
-  document.getElementById('check-room2').checked=false;
-  document.getElementById('check-room1').checked=false;
-  }else{
-  document.getElementById('check-room2').checked=true;
-  document.getElementById('check-room1').checked=true;
-  }
-
-});
-*/
-
