cb8271f63abca70f7233a50ee0b67daf3c0d2246
CarlaOnate <carlaonategardella@gmail.com>
2020-02-13 10:18:18 -0600

    Fixing some issues, admin profile ready again

--- starter-code/controllers/UDprofile.controller.js
+++ starter-code/controllers/UDprofile.controller.js
@@ -15,8 +15,18 @@ exports.editProfile = async (req, res) => {
 }
 
 exports.editProfileView = async (req, res) => {
+  if(req.user.role === 'ADMIN'){
+    let actAll = await Activity.find().populate('user_id')
+    let activities = await Activity.find().populate('user_id')
+    let reservations = await Reservation.find().populate('user_id')
+    let user = await User.findById(req.user._id)
+    res.render('profile', {activities, reservations, user, show:false, profile:true, admin:true})
+  } else {
+  let activities = await Activity.find({user_id: req.user._id})
+  let reservations = await Reservation.find({user_id: req.user._id})
   let user = await User.findById(req.user._id)
   res.render('edit-profile', {user,  show:false, profile:true})
+}
 }
 
 exports.deleteProfile = async (req, res) => {
